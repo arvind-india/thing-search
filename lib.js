be6443be8b25cc653be31ce4bcb79d983b41a8ad
Alf Eaton <alf@hubmed.org>
2014-01-18 10:38:48 +0000

    better parsing

--- lib.js
+++ lib.js
@@ -1,4 +1,80 @@
-function durationToTime(duration) {
+var console = console || { log: function() {}};
+
+var parseSnippet = function (snippet) {
+	console.log(snippet);
+
+	return {
+		artist: parseArtist(snippet),
+		album: parseAlbum(snippet),
+		date: parseDate(snippet),
+		description: parseDescription(snippet),
+		image: parseImage(snippet)
+	}
+}
+
+var parseArtist = function (snippet) {
+	if (snippet.musicalbum && snippet.musicalbum.byartist) {
+		if (typeof snippet.musicalbum.byartist == 'string') {
+			return snippet.musicalbum.byartist;
+		}
+
+		if (typeof snippet.musicalbum.byartist == 'object') {
+			if (snippet.musicalbum.byartist.name) {
+				return snippet.musicalbum.byartist.name;
+			}
+		}
+	}
+
+	if (snippet.musicgroup && snippet.musicgroup.name) {
+		return snippet.musicgroup.name;
+	}
+}
+
+var parseAlbum = function (snippet) {
+	if (snippet.musicalbum && snippet.musicalbum.name) {
+		return snippet.musicalbum.name;
+	}
+}
+
+var parseDescription = function (snippet) {
+	if (snippet.musicalbum && snippet.musicalbum.description) {
+		return snippet.musicalbum.description;
+	}
+}
+
+var parseImage = function (snippet) {
+	if (snippet.musicalbum && snippet.musicalbum.image) {
+		return {
+			src: snippet.musicalbum.image
+		}
+	}
+
+	return snippet.cseThumbnail;
+}
+
+var parseDate = function (snippet) {
+	var date;
+
+	if (snippet.musicalbum && snippet.musicalbum.datepublished) {
+		date = snippet.musicalbum.datepublished;
+	} else if (snippet.metatags && snippet.metatags.musicReleaseDate) {
+		date = snippet.metatags.musicReleaseDate;
+	}
+
+	if (!date) {
+		return;
+	}
+
+	date = date.replace(/-/, '');
+
+	var matches = date.match(/^(\d{4})/);
+
+	if (matches) {
+		return ' (' + matches[1] + ')';
+	}
+}
+
+var parseDuration = function (duration) {
 	if (!duration) {
 		return '';
 	}
